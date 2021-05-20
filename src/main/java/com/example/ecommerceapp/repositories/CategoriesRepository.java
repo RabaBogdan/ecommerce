932ea7c1a89866d74c1733e961de0a06366518b2
rababogdan <rababobo@yahoo.com>
2021-05-20 22:29:17 +0300

    Update

--- src/main/java/com/example/ecommerceapp/repositories/CategoriesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommerceapp.repositories;
-
-import com.example.ecommerceapp.entities.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriesRepository extends JpaRepository<ProductCategory, Long> {
-    ProductCategory findById(long id);
-}
--- src/main/java/com/example/ecommerceapp/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.ecommerceapp.repositories;
-
-import com.example.ecommerceapp.entities.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Products, Long> {
-    Products findById(long id);
-}
